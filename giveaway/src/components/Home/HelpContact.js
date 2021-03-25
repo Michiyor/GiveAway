f692cf8393c1d82a5cd4539c17eea43a11f8aef8
Michiyor <joanna.rutkowska96@gmail.com>
2021-03-25 20:03:49 +0100

    add new section

---
+++ giveaway/src/components/Home/HelpContact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HelpContact() {
+    return(
+        <section className="helpContact" id="contact">
+
+        </section>
+    )
+}
--- giveaway/src/components/Home/HomeHelp.js
+++ giveaway/src/components/Home/HomeHelp.js
@@ -1,16 +1,26 @@
 import React from "react";
 
 function HomeHelp() {
-    return(
+    return (
         <section className="homeHelp" id="help">
             <h2 className="title">Komu pomagamy?</h2>
             <ul className="help__list">
-                <li><button className="help__btn">Fundacjom</button></li>
-                <li><button className="help__btn">Organizacjom pozarządowym</button></li>
-                <li><button className="help__btn">Lokalnym zbiórkom</button></li>
+                <li>
+                    <button className="help__btn">Fundacjom</button>
+                </li>
+                <li>
+                    <button className="help__btn">Organizacjom pozarządowym</button>
+                </li>
+                <li>
+                    <button className="help__btn">Lokalnym zbiórkom</button>
+                </li>
             </ul>
-            <p className="help__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation.</p>
+            <div className="help__fundations container">
+                <p className="help__text">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z<br/>
+                    którymi współpracujemy. Możesz sprawdzić czym się zajmują,<br/>
+                    komu pomagają i czego potrzebują.
+                </p>
+            </div>
         </section>
     )
 }
