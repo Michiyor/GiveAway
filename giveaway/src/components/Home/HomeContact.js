f7bc8fffbd4720cb3222d48eee147c5da7d45f45
Michiyor <joanna.rutkowska96@gmail.com>
2021-03-31 22:40:26 +0200

    broken form ver2

--- giveaway/src/components/Home/HomeContact.js
+++ giveaway/src/components/Home/HomeContact.js
@@ -29,20 +29,23 @@ const HomeContact = () => {
     };
     return (
         <>
-            <section className="homeContact" id="contact">
-                <div className="contact__picture">
-                    <img className="image contact__image" src={blankets} alt="blanket"/>
+            <section className="homeContact" id="contact" style={{backgroundImage: `url(${blankets})`}}>
+                <div className="contact__main">
+                    <form className="contact__form" onSubmit={handleSubmit}>
+                        <h3 className="title">Skontaktuj się z nami</h3>
+                        <label htmlFor="name"> Wpisz swoje imię</label>
+                        <input id="name" placeholder="Jan" type="text" name="name" value={name.name} onChange={handleChange}/>
+                        <label htmlFor="email">Wpisz swojego emaila </label>
+                        <input id="email" placeholder="jan.nowak@gmail.com" type="text" name="email" value={email.name}
+                               onChange={handleChangeEmail}/>
+                        <label htmlFor="msg">Wpisz swoją wiadomość</label>
+                        <textarea id="msg"
+                            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            cols="50" rows="6">
+                    </textarea>
+                        <input type="submit" value="Wyślij"/>
+                    </form>
                 </div>
-                <form className="contact__form" onSubmit={handleSubmit}>
-                    <h3 className="title">Skontaktuj się z nami</h3>
-                    <label> Wpisz swoje imię</label>
-                    <input type="text" name="name" value={name.name} onChange={handleChange}/>
-                    <label>Wpisz swojego emaila</label>
-                    <input type="text" name="email" value={email.name} onChange={handleChangeEmail}/>
-                    <label>Wpisz swoją wiadomość</label>
-                    <textarea cols="50" rows="4">.</textarea>
-                    <input type="submit" value="Wyślij"/>
-                </form>
             </section>
         </>
     );
