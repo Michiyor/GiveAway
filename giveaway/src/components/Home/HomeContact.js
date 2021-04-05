a58eb70e532b0efbb6084e8c8a45842299d04cae
Michiyor <joanna.rutkowska96@gmail.com>
2021-04-05 20:18:05 +0200

    done contact

--- giveaway/src/components/Home/HomeContact.js
+++ giveaway/src/components/Home/HomeContact.js
@@ -33,17 +33,19 @@ const HomeContact = () => {
                 <div className="contact__main">
                     <form className="contact__form" onSubmit={handleSubmit}>
                         <h3 className="title">Skontaktuj się z nami</h3>
-                        <label htmlFor="name"> Wpisz swoje imię</label>
-                        <input id="name" placeholder="Jan" type="text" name="name" value={name.name} onChange={handleChange}/>
-                        <label htmlFor="email">Wpisz swojego emaila </label>
-                        <input id="email" placeholder="jan.nowak@gmail.com" type="text" name="email" value={email.name}
+                        <div className="contact__labels">
+                            <label className=" label" htmlFor="name"> Wpisz swoje imię</label>
+                            <label className=" label" htmlFor="email">Wpisz swojego emaila </label>
+                        </div>
+                        <input className="contact__el" id="name" placeholder="Jan" type="text" name="name" value={name.name} onChange={handleChange}/>
+                        <input className="contact__el" id="email" placeholder="jan.nowak@gmail.com" type="text" name="email" value={email.name}
                                onChange={handleChangeEmail}/>
-                        <label htmlFor="msg">Wpisz swoją wiadomość</label>
-                        <textarea id="msg"
+                        <label className="contact__msg-label" htmlFor="msg">Wpisz swoją wiadomość</label>
+                        <textarea id="msg" className="contact__msg"
                             placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
                             cols="50" rows="6">
                     </textarea>
-                        <input type="submit" value="Wyślij"/>
+                        <input className="contact__btn" type="submit" value="Wyślij"/>
                     </form>
                 </div>
             </section>
